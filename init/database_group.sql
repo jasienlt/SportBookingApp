e75e7bb292894b65239d637c249db0b8fc1441a6
Jasie Ngo <48340091+jasienlt@users.noreply.github.com>
2024-06-10 19:26:34 +1000

    Re-construct file structure to match up with main

--- init/database_group.sql
+++ init/database_group.sql
@@ -1,19 +1,21 @@
-DROP DATABASE sport_app;
+-- use this if error logged that database exists
+-- DROP DATABASE sport_app;
+
 CREATE DATABASE sport_app;
 use sport_app;
 
 CREATE TABLE sportgroup (
-                            id bigint NOT NULL AUTO_INCREMENT
+                            id int NOT NULL
     , name varchar(100) NOT NULL
     , PRIMARY KEY (id)
 );
 
 CREATE TABLE court(
-                      id bigint NOT NULL AUTO_INCREMENT
+                      id int NOT NULL
     , name varchar(100) NOT NULL
     , address varchar(1000) NULL
     , phone varchar(10) NULL
-    , sportgroup_id bigint
+    , sportgroup_id int
     , PRIMARY KEY (id)
     , INDEX sportgroup_idx (sportgroup_id)
     , FOREIGN KEY (sportgroup_id) REFERENCES sportgroup(id)
@@ -24,12 +26,11 @@ CREATE TABLE court(
 );
 
 CREATE TABLE product (
-                         id bigint NOT NULL AUTO_INCREMENT
+                         id int NOT NULL PRIMARY KEY AUTO_INCREMENT
     , name varchar(100) NOT NULL
     , price float NOT NULL
     , amount int NOT NULL
-    , PRIMARY KEY (id)
-    , court_id bigint
+    , court_id int
     , INDEX prod_court_idx (court_id)
     , FOREIGN KEY (court_id) REFERENCES court(id)
                              ON
@@ -39,18 +40,17 @@ CREATE TABLE product (
 );
 
 CREATE TABLE customer (
-    id bigint NOT NULL AUTO_INCREMENT
+                          id int NOT NULL
     , first_name varchar(100) NOT NULL
     , last_name varchar(100) NOT NULL
-    , phone varchar(15) NULL
+    , phone varchar(10) NULL
     , email varchar(1000) NULL
-    , password varchar(1000) NULL
     , PRIMARY KEY (id)
 );
 
-CREATE TABLE courtCustomer (
-                                court_id bigint NOT NULL
-    , customer_id bigint NOT NULL
+CREATE TABLE court_customer (
+                                court_id int NOT NULL
+    , customer_id int NOT NULL
     , loyalty_point float NULL
     , INDEX cc_court_idx (court_id)
     , FOREIGN KEY (court_id) REFERENCES court(id)
@@ -68,10 +68,10 @@ CREATE TABLE courtCustomer (
 
 
 CREATE TABLE field (
-                       id bigint NOT NULL AUTO_INCREMENT
+                       id int NOT NULL
     , name varchar(100) NOT NULL
     , sport_type varchar(100) NOT NULL
-    , court_id bigint
+    , court_id int
     , PRIMARY KEY (id)
     , INDEX f_court_idx (court_id)
     , FOREIGN KEY (court_id) REFERENCES court(id)
@@ -82,10 +82,10 @@ CREATE TABLE field (
 );
 
 CREATE TABLE timeslot(
-                         id bigint NOT NULL AUTO_INCREMENT
+                         id int NOT NULL
     , start_time time
     , end_time time
-    , court_id bigint
+    , court_id int
     , PRIMARY KEY (id)
     , INDEX ts_court_idx (court_id)
     , FOREIGN KEY (court_id) REFERENCES court (id)
@@ -95,9 +95,9 @@ CREATE TABLE timeslot(
                              ON DELETE CASCADE
 );
 
-CREATE TABLE fieldTimeslot (
-                                field_id bigint NOT NULL
-    , ts_id bigint NOT NULL
+CREATE TABLE field_timeslot (
+                                field_id int NOT NULL
+    , ts_id int NOT NULL
     , price float NOT NULL
     , day_in_week int NOT NULL
     , INDEX fts_field_id (field_id)
@@ -115,18 +115,19 @@ CREATE TABLE fieldTimeslot (
 );
 
 CREATE TABLE payment(
-                        id bigint NOT NULL AUTO_INCREMENT
+                        id int NOT NULL
     , payment_type int NOT NULL
     , PRIMARY KEY (id)
 );
 
 CREATE TABLE booking (
-                         id bigint NOT NULL AUTO_INCREMENT
+                         id int NOT NULL
     , date date
-    , customer_id bigint
-    , field_id bigint
-    , ts_id bigint
-    , p_id bigint
+    , customer_id int
+    , field_id int
+    , ts_id int
+    , p_id int
+    , price float
     , PRIMARY KEY (id)
     , INDEX b_customer_id (customer_id)
     , FOREIGN KEY (customer_id) REFERENCES customer(id)
@@ -159,23 +160,25 @@ CREATE TABLE booking (
 
 -- 1) Group
 INSERT INTO
-    sportgroup(name)
+    sportgroup(id, name)
 VALUES
-('Dung Cau Long')
-     , ('Vuong Bong Ro');
+(1, 'Dung Cau Long')
+     , (2, 'Vuong Bong Ro');
 
 -- 2) Court
 INSERT INTO
-    court(name, address, phone, sportgroup_id)
+    court(id, name, address, phone, sportgroup_id)
 VALUES
 (
-  'Dung Cau Long - Chi nhanh 1'
+  1
+, 'Dung Cau Long - Chi nhanh 1'
 , 'abc xyz'
 , '0123456789'
 , 1
 )
      , (
-         'Dung Cau Long - Chi nhanh 2'
+         2
+       , 'Dung Cau Long - Chi nhanh 2'
        , 'def ghi'
        , '0987654321'
        , 1
@@ -183,69 +186,72 @@ VALUES
 
 -- 3) Product
 INSERT INTO
-    product(name, price, amount, court_id)
+    product(id, name, price, amount, court_id)
 VALUES
-('Sting Dau', 8000, 1, 1)
-     , ('Pepsi', 10000, 1, 1)
-     , ('7Up', 10500, 1, 1);
+(1, 'Sting Dau', 8000, 1, 1)
+     , (2, 'Pepsi', 10000, 1, 1)
+     , (3, '7Up', 10500, 1, 1);
 
 -- 4) Customer
 INSERT INTO
-    customer(first_name, last_name, phone, email, password)
+    customer(id, first_name, last_name, phone, email)
 VALUES
 (
-  'Dung'
+  1
+, 'Dung'
 , 'Nguyen'
 , 0112349395
 , 'dung.nguyen@yahoo.com'
-, 'dung_nguyen'
 )
      , (
-         'Vuong'
+         2
+       , 'Vuong'
        , 'Tran'
        , 0482039493
        , 'vuong.tran@hotmail.com'
-       , 'vuong_tran'
 );
 
 -- 5) Court_Customer
 INSERT INTO
-    courtCustomer(court_id, customer_id, loyalty_point)
+    court_customer(court_id, customer_id, loyalty_point)
 VALUES
 (1, 1, 500)
      , (1, 2, 300);
 
 -- 6) Field
 INSERT INTO
-    field(name, sport_type, court_id)
+    field(id, name, sport_type, court_id)
 VALUES
-('San 1', 1, 1)
-     , ('San 2', 1, 1)
-     , ('San 3', 1, 1);
+(1, 'San 1', 1, 1)
+     , (2, 'San 2', 1, 1)
+     , (3, 'San 3', 1, 1);
 
 -- 7) Time Slot
 INSERT INTO
-    timeslot(start_time, end_time, court_id)
+    timeslot(id, start_time, end_time, court_id)
 VALUES
 (
-  str_to_date('0500', '%h%i')
+  1
+, str_to_date('0500', '%h%i')
 , str_to_date('0530', '%h%i')
 , 1
 )
      , (
-         str_to_date('0530', '%h%i')
+         2
+       , str_to_date('0530', '%h%i')
        , str_to_date('0600', '%h%i')
        , 1
 )
      , (
-         str_to_date('0600', '%h%i')
+         3
+       , str_to_date('0600', '%h%i')
        , str_to_date('0630', '%h%i')
        , 1
 );
 
 -- 8) Field - Timeslot
 INSERT INTO
-    fieldTimeslot(field_id, ts_id, price, day_in_week)
+    field_timeslot(field_id, ts_id, price, day_in_week)
 VALUES
 (1, 1, 50000, 1)
      , (1, 2, 50000, 1)
@@ -253,15 +259,15 @@ VALUES
 
 -- 9) Payment
 INSERT INTO
-    payment(payment_type)
+    payment(id, payment_type)
 VALUES
-(1)
-     , (2)
-     , (1);
+(1, 1)
+     , (2, 2)
+     , (3, 1);
 
 -- 10) Booking
 INSERT INTO
-    booking(date, customer_id, field_id, ts_id, p_id)
+    booking(id, date, customer_id, field_id, ts_id, p_id, price)
 VALUES
-(str_to_date('2024-05-01', '%Y-%m-%d'), 1, 1, 1, 1)
-     , (str_to_date('2024-05-01', '%Y-%m-%d'), 1, 1, 2, 1);
+(1, str_to_date('2024-05-01', '%Y-%m-%d'), 1, 1, 1, 1, 50)
+     , (2, str_to_date('2024-05-01', '%Y-%m-%d'), 1, 1, 2, 1, 100);
