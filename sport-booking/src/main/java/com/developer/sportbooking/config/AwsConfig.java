5bf0f7412dded053523b3b9c64681088047ebad0
Jasie Ngo <48340091+jasienlt@users.noreply.github.com>
2024-08-09 15:18:07 +1000

    Update AwsConfig.java

--- sport-booking/src/main/java/com/developer/sportbooking/config/AwsConfig.java
+++ sport-booking/src/main/java/com/developer/sportbooking/config/AwsConfig.java
@@ -1,28 +1,203 @@
 package com.developer.sportbooking.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.awspring.cloud.s3.InMemoryBufferingS3OutputStreamProvider;
-import io.awspring.cloud.s3.Jackson2JsonS3ObjectConverter;
-import io.awspring.cloud.s3.S3Template;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.*;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.core.waiters.WaiterResponse;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.model.*;
+import software.amazon.awssdk.services.s3.waiters.S3Waiter;
 
-@Configuration
+import javax.imageio.ImageIO;
+
+
 public class AwsConfig {
-    @Bean
-    public S3Client s3Client(){
-        return S3Client.builder()
+
+    private static final String BUCKET = "sportbookingapp";
+    private static final String folderName1 = "payment_screenshots";
+    private static final String folderName2 = "court_url";
+
+    //@Value("${aws.accessKeyId}")
+    private static String accessKeyId = "";
+
+    //@Value("${aws.secretAccessKey}")
+    private static String secretAccessKey = "";
+
+    public static String folderDirect(Integer folderNum) {
+        return switch (folderNum) {
+            case 1 -> folderName1;
+            case 2 -> folderName2;
+            default -> null;
+        };
+    }
+
+    // PUT
+
+    public static void uploadFile(String fileName, InputStream inputStream, String folderDir)
+            throws AwsServiceException, SdkClientException, IOException {
+
+
+        AwsCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+        S3Client client = S3Client.builder()
                 .region(Region.AP_SOUTHEAST_2)
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create({accessKeyId}, {accessKeyId1}))).build();
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+
+
+
+        String key = folderDir + "/" + fileName;
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(key)
+                .contentType("image/*")
+                .build();
+
+        client.putObject(request,
+                RequestBody.fromInputStream(inputStream, inputStream.available()));
+
+
+        S3Waiter waiter = client.waiter();
+        HeadObjectRequest waitRequest = HeadObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(key)
+                .build();
+
+        WaiterResponse<HeadObjectResponse> waitResponse = waiter.waitUntilObjectExists(waitRequest);
+
+        waitResponse.matched().response().ifPresent(System.out::println);
+        System.out.println("File " + fileName + " was uploaded.");
     }
-    @Bean
-    public S3Template s3Template(){
-        return new S3Template(s3Client() , new InMemoryBufferingS3OutputStreamProvider(s3Client() , null) ,new Jackson2JsonS3ObjectConverter(new ObjectMapper()), S3Presigner.create());
+
+    public static void createFolder(String folderName, String folderDir)
+            throws AwsServiceException, SdkClientException, IOException {
+
+
+        AwsCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+        S3Client client = S3Client.builder()
+                .region(Region.AP_SOUTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+
+
+
+        String key = folderDir + "/" + folderName + "/";
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(key)
+                .contentType("image/*")
+                .build();
+
+        client.putObject(request,
+                RequestBody.empty());
+
+
+        S3Waiter waiter = client.waiter();
+        HeadObjectRequest waitRequest = HeadObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(key)
+                .build();
+
+        WaiterResponse<HeadObjectResponse> waitResponse = waiter.waitUntilObjectExists(waitRequest);
+
+        waitResponse.matched().response().ifPresent(System.out::println);
+        System.out.println("Folder " + folderName + " was uploaded.");
     }
+
+    // COPY & DELETE
+    public static void moveFile(String fileName, String fromDir, String toDir) {
+        AwsCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+        S3Client client = S3Client.builder()
+                .region(Region.AP_SOUTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+
+
+
+        // Copy file into the new directory
+
+        String fromKey = fromDir + "/" + fileName;
+        String toKey = toDir + "/" + fileName;
+        CopyObjectRequest request = CopyObjectRequest.builder()
+                .sourceBucket(fromKey)
+                .destinationBucket(toKey)
+                .build();
+        client.copyObject(request);
+
+        S3Waiter waiter = client.waiter();
+        HeadObjectRequest waitRequest = HeadObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(toKey)
+                .build();
+
+        WaiterResponse<HeadObjectResponse> waitResponse = waiter.waitUntilObjectExists(waitRequest);
+
+        waitResponse.matched().response().ifPresent(System.out::println);
+        System.out.println("File " + fileName + " was moved successfully.");
+
+        // Delete old file
+
+        DeleteObjectRequest request1 = DeleteObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(fromKey)
+                .build();
+        client.deleteObject(request1);
+
+    }
+
+    // GET
+
+    public static String getImage(String fileName, Integer folderNum)
+            throws AwsServiceException, SdkClientException, IOException {
+
+
+        AwsCredentials credentials = AwsBasicCredentials.create(accessKeyId, secretAccessKey);
+        S3Client client = S3Client.builder()
+                .region(Region.AP_SOUTHEAST_2)
+                .credentialsProvider(StaticCredentialsProvider.create(credentials))
+                .build();
+
+
+
+        String key = folderDirect(folderNum) + "/" + fileName;
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(key)
+                .build();
+
+        ResponseBytes<GetObjectResponse> objectBytes = client.getObjectAsBytes(request);
+        byte[] data = objectBytes.asByteArray();
+
+        // create the object of ByteArrayInputStream class
+        // and initialized it with the byte array.
+        // ByteArrayInputStream inStreambj = new ByteArrayInputStream(data);
+
+        // read image from byte array
+        // BufferedImage finalImage = ImageIO.read(inStreambj);
+
+        String imageAsBase64 = Base64.getEncoder().encodeToString(data);
+
+        S3Waiter waiter = client.waiter();
+        HeadObjectRequest waitRequest = HeadObjectRequest.builder()
+                .bucket(BUCKET)
+                .key(key)
+                .build();
+
+        WaiterResponse<HeadObjectResponse> waitResponse = waiter.waitUntilObjectExists(waitRequest);
+
+        waitResponse.matched().response().ifPresent(System.out::println);
+        System.out.println("Folder " + fileName + " was successfully retrieved.");
+        return imageAsBase64;
+    }
+
 }
