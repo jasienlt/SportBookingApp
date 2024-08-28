2f87e172ab2518e85293d55a9b43c47d2913eb08
Jasie Ngo <ngolamthy@gmail.com>
2024-08-28 13:00:21 +1000

    Updated code for Controllers

--- sport-booking/src/main/java/com/developer/sportbooking/service/CourtService.java
+++ sport-booking/src/main/java/com/developer/sportbooking/service/CourtService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public interface CourtService {
+public interface git CourtService {
     Court saveCourt(CourtDto courtDto);
 
     List<Court> findAllCourt();
